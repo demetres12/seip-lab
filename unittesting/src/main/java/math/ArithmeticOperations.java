c434242dd5bbe04823943ac01a477cfbfeb5a9f9
Dimitrios Kokkotas <dimitriskokkotas@gmail.com>
2023-05-09 17:27:04 +0300

    Update ArithmeticOperations.java class

--- unittesting/src/main/java/math/ArithmeticOperations.java
+++ unittesting/src/main/java/math/ArithmeticOperations.java
@@ -1,26 +1,38 @@
 package math;
 
 /**
-* The MathOperations provides simple arithmetic operations
-* that serve as hands-on practice on Unit Testing.
-*
-* @author  agkortzis
-* @version 1.0
-* @since   2020-04-06 
-*/
+ * The MathOperations provides simple arithmetic operations
+ * of division and multiplication that serve as hands-on
+ * practice on Unit Testing.
+ *
+ * @author  agkortzis, dkokkotas
+ * @version 1.0
+ * @since   2020-04-06 
+ */
 public class ArithmeticOperations {
 	
 	/**
 	 * Performs the basic arithmetic operation of division.
+	 * Generally, division by a zero denominator throws
+	 * an ArithmeticException. However inside divide method
+	 * we include another way of handling such cases.
+	 * Specifically when both numerator and denominator are
+	 * zero values, we return a Nan(Not a Number) value
+	 * instead of throwing the exception. We could also take
+	 * into consideration the infinity concept of Java, in
+	 * case only the denominator is a zero value, but this
+	 * could add unwanted complexity to our project.
+	 * 
 	 * @param numerator the numerator of the operation
 	 * @param denominator the denominator of the operation
 	 * @return the result of the division between numerator and denominator
-	 * @exception ArithmeticException when denominator is zero 
+	 * @exception ArithmeticException when denominator is zero but numerator is not
 	 */
 	public double divide(double numerator, double denominator) {
 		// Use compare() method of Double class for more precise comparisons
 		if ((Double.compare(numerator, 0.0) == 0)
 				&& (Double.compare(denominator, 0.0) == 0)) {
+			// Division zero by zero returns Not a Number value
 			return Double.NaN;
 		} else if ((Double.compare(numerator, 0.0) != 0)
 				&& (Double.compare(denominator, 0.0) == 0)) {
@@ -34,8 +46,9 @@ public class ArithmeticOperations {
 	/**
 	 * Performs the basic arithmetic operation of multiplication 
 	 * between two positive Integers
-	 * @param x the first input
-	 * @param y the second input
+	 * 
+	 * @param x the first input, that being a positive integer
+	 * @param y the second input, that being a positive integer
 	 * @return the product of the multiplication
 	 * @exception IllegalArgumentException when <b>x</b> or <b>y</b> are negative numbers 
 	 * @exception IllegalArgumentException when the product does not fit in an Integer variable 
@@ -43,7 +56,9 @@ public class ArithmeticOperations {
 	public int multiply(int x, int y) {
 		if (x < 0 || y < 0) {
 			throw new IllegalArgumentException("x & y should be >= 0");
-		// Otherwise, ArithmeticException will be thrown as y is used as denominator
+		/* Otherwise, ArithmeticException will be thrown
+		 * as y is used as denominator and must no be equal to zero
+		 */
 		} else if (y == 0) {
 			return x * y;
 		} else if (x <= divide(Integer.MAX_VALUE, y)) {
@@ -52,6 +67,4 @@ public class ArithmeticOperations {
 			throw new IllegalArgumentException("The product does not fit in an Integer variable");		
 		}
 	}
-	
-	
 }
